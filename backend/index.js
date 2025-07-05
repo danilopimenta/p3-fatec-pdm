49285c3195f60fafc0d91a13e55fc66547f4060d
Danilo Pimenta <danilopi@outlook.com>
2025-07-04 21:40:02 -0300

    feat(p3): cria endpoint do backend e separação de backend/frontend

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+const axios = require('axios')
+const cors = require('cors')
+const express = require('express')
+const app = express()
+
+const OPEN_WEATHER_MAP_KEY = process.env.OPEN_WEATHER_MAP_KEY
+
+app.use(cors())
+const openWeatherMapClient = axios.create({
+    baseURL: 'https://api.openweathermap.org/data/2.5',
+})
+
+app.get('/weather', async (req, res) => {
+    const {lat, lon} = req.query
+    try {
+        const appid = OPEN_WEATHER_MAP_KEY
+        const endpoint = '/weather'
+        const params = { params: {lat, lon, appid} }
+        const result = await openWeatherMapClient.get(endpoint, params)
+        res.json(result.data)
+    } catch (err) {
+        console.log(err)
+        res.status(400)
+        res.json({ error: err, msg: "erro no backend" })
+    }
+})
+
+const port = 3000
+app.listen(port, () => console.log(`Back End OK! PORT ${port}.`))
